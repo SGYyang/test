d39e033c90a0bac607df58ca3c9c5c4cd710a16c
SGYyang <gyy001213@126.com>
2018-12-08 02:49:46 +0800

    初次见面

---
+++ git-test/src/main/java/com/yang/test/Test.java
@@ -0,0 +1,14 @@
+package com.yang.test;
+
+/**
+ * @auther gyy
+ * create 2018/12/8.
+ */
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
